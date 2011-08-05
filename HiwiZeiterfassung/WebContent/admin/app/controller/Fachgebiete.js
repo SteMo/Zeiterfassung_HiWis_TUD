31a14d0a7cacc838a379d3f8fc04d9265d3b0086
Stephan <web@moczygemba.de>
2011-08-05 16:22:21 +0200

    Adminansicht: Fachgebiete erstellt

---
+++ HiwiZeiterfassung/WebContent/admin/app/controller/Fachgebiete.js
@@ -0,0 +1,31 @@
+Ext.define('AM.controller.Fachgebiete', {
+    extend: 'Ext.app.Controller',
+
+    stores: ['Fachgebiete','Personentypen','Tabelle1'],
+	
+    views: [
+        'menu.Menu',
+        'menu.FachgebietMenuBottom',
+        'fachgebiete.SearchField',
+		'fachgebiete.ComboFachgebiete',
+		'fachgebiete.ComboPersonentypen',
+		'fachgebiete.ContentGrid'
+    ],
+	
+	models: [
+		'fachgebiete.Tabelle1',
+		'fachgebiete.Personentypen',
+		'fachgebiete.Fachgebiete'
+	],
+	
+
+    init: function() {
+    },
+
+
+    // editUser: function(grid, record) {
+        // var view = Ext.widget('useredit');
+
+        // view.down('form').loadRecord(record);
+    // }
+});
---
+++ HiwiZeiterfassung/WebContent/admin/app/model/fachgebiete/Tabelle1.js
@@ -0,0 +1,4 @@
+Ext.define('AM.model.fachgebiete.Tabelle1', {
+    extend: 'Ext.data.Model',
+    fields:['id', 'fachgebiet', 'kuerzel', 'leiter', 'stellvertreter']
+});
