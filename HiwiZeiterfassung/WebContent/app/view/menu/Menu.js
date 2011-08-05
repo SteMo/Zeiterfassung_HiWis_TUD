caa321ded1dfa2a77d1e8fac48919d0c32709032
Fabian Letzkus <f.letzkus@gmail.com>
2011-08-05 16:52:40 +0200

    Menuestruktur ist nun Datenbankabhaengig und kann per AJAX und je nach Benutzer geladen werden

--- HiwiZeiterfassung/WebContent/app/view/menu/Menu.js
+++ HiwiZeiterfassung/WebContent/app/view/menu/Menu.js
@@ -4,35 +4,68 @@
 //	]);
 //
 //
-//
+// 	
 
+var createItemListFromStore = function (dataStore) {
+	var itemList = new Array();
+	var i = 0;
+	for(var menuItem in dataStore.data.items) {
+		console.log(">>>>> "+menuItem);
+		itemList[i] = new Object();
+		for(var menuItemKey in dataStore.data.items[menuItem].fields.keys) {
+			itemList[i][dataStore.data.items[menuItem].fields.keys[menuItemKey]] = dataStore.data.items[menuItem].data[dataStore.data.items[menuItem].fields.keys[menuItemKey]]
+		}
+		i++;
+	}
+	return itemList;
+}
+
+Ext.define('menuModel', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'text', type: 'string'},
+        {name: 'handler',type: 'string'},
+        {name: 'xtype',type: 'string'}
+    ]
+});
 
 Ext.define('AM.view.menu.Menu' ,{
     alias : 'widget.menue',
     extend: 'Ext.Toolbar',
     
-    
     autoRender: true,
    
 	title: '',
 	width: 700,
+	
+	items: createItemListFromStore(new Ext.data.Store({
+	    model: 'menuModel',
+	    data : [
+	        {text: 'Dashboard',handler:'',xtype: 'button'},
+	        {text: 'Fachbereich',handler:'',xtype: 'button'},
+	        {text: 'Personen',handler:'',xtype: 'button'},
+	        {text: 'Vertr&auml;ge',handler:'',xtype: 'button'},
+	        {text: 'Aufgaben',handler:'',xtype: 'button'}
+	    ]
+	})),
+	
 
-	items: [
-		{	text: 'Dashboard'	},
-		' ',
-		{	text: 'Fachgebiete'	},
-		' ',
-		{	text: 'Personen', handler: function() {
-								window.location.href = "index.html";
-			}},
-		' ',
-		{	text: 'Vertr‰ge', handler: function() {
-								window.location.href = "vertraege.html";
-			}},
-		' ',
-		{	text: 'Aufgaben'	},
-		' '
-	],  
+//	items: [
+//		{	text: 'Dashboard'	},
+//		' ',
+//		{	text: 'Fachgebiete'	},
+//		' ',
+//		{	text: 'Personen', handler: function() {
+//								window.location.href = "index.html";
+//			}},
+//		' ',
+//		{	text: 'Vertr√§ge', handler: function() {
+//								window.location.href = "vertraege.html";
+//			}},
+//		' ',
+//		{	text: 'Aufgaben'	},
+//		' '
+//	],  
 	
 //	 initComponent: function() {
 //	   id = Ext.util.Cookies.get('id');
