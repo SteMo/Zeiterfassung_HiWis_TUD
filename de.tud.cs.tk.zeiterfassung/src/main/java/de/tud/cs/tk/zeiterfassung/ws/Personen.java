fda7fb34b1d702dc496da4167717255712e1fff7
Fabian Letzkus <f.letzkus@gmail.com>
2011-08-29 22:27:36 +0200

    Resultierendes JSON hinzugefuegt

--- de.tud.cs.tk.zeiterfassung/src/main/java/de/tud/cs/tk/zeiterfassung/ws/Personen.java
+++ de.tud.cs.tk.zeiterfassung/src/main/java/de/tud/cs/tk/zeiterfassung/ws/Personen.java
@@ -4,6 +4,12 @@
  */
 package de.tud.cs.tk.zeiterfassung.ws;
 
+import de.tud.cs.tk.zeiterfassung.dao.FachgebietDAO;
+import de.tud.cs.tk.zeiterfassung.dao.PersonDAO;
+import de.tud.cs.tk.zeiterfassung.entities.Person;
+
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.*;
 
 /**
@@ -12,9 +18,48 @@ import javax.ws.rs.*;
  */
 @Path("/personen")
 public class Personen {
-    
+
+    public class PersonList {
+
+        public boolean success;
+        public int total;
+        public List<PersonView> results = new ArrayList<PersonView>();
+    }
+
+    public class PersonView {
+
+        public String name, fachgebiet, position;
+        public long id, supervisor;
+
+        public PersonView(long id, String name, String fg, String pos, long sv) {
+            this.id = id;
+            this.name = name;
+            this.fachgebiet = fg;
+            this.position = pos;
+            this.supervisor = sv;
+        }
+    }
+
     @GET
-    public String getId() {
-        return "hello-world";
+    @Produces({"application/json"})
+    public PersonList getPeople() {
+        PersonList pl = new PersonList();
+        List<Person> peopleDAO = PersonDAO.retrieveAll();
+        pl.total = peopleDAO.size();
+        for (Person p : peopleDAO) {
+            pl.results.add(new PersonView(p.id, p.firstName + " " + p.givenName,
+                    p.getFachgebiet().name,
+                    null,
+                    (p.getSupervisor() != null)
+                    ? p.getSupervisor().id 
+                    : -1));
+        }
+        return pl;
+    }
+
+    @GET
+    @Path("/{ident}")
+    public Person getById(@PathParam("ident") String ident) {
+        return null;
     }
 }
