5403a5084d8db9661d910e732be691da7951dbb3
Fabian Letzkus <f.letzkus@gmail.com>
2011-09-21 02:04:21 +0200

    JsonP-Proxy überschrieben

--- de.tud.cs.tk.zeiterfassung/src/main/webapp/app/model/Models.js
+++ de.tud.cs.tk.zeiterfassung/src/main/webapp/app/model/Models.js
@@ -1,177 +1,6 @@
 /* Warum hier proxy? Setzt Standardproxy, muss dann nicht von Stores immer gesetzt werden, nur wenn mehrere Stores selbes model benutzen und sich RÃ¼ckgabe unterscheidet  */
 
-/**
- * ! Wenn sich etwas am Model Ã¤ndert mÃ¼ssen evtl auch andere Stellen angepasst werden, da diese 
- * auf die Felder ja zugreifen (z.B. die Detail-Fenster zu Personen bei Doppelklick @ Controller) 
- * */
-
-Ext.define('AM.model.Fachgebiete', {
-    extend: 'Ext.data.Model',
-    fields:['name'],
-	proxy: {
-	    type: 'jsonp',
-	    url: 'ws/fachgebiete',
-	    reader: {
-	        type: 'json',
-	        root: 'results'
-	    }
-	}
-});
-
-Ext.define('AM.model.Personentypen', {
-    extend: 'Ext.data.Model',
-    fields:['name'],
-    proxy: {
-        type: 'ajax',
-        url: 'resources/data/personentypen.json',
-        reader: {
-            type: 'json',
-            root: 'results'
-        }
-    }
-});
-
-
-Ext.define('AM.model.Personen', {
-    extend: 'Ext.data.Model',
-    fields:['id', 'name', 'fachgebiet', 'position', 'supervisor'],
-    proxy: {
-        type: 'jsonp',
-        url: 'ws/personen',
-        format: 'json',
-       // reader: {
-       //     type: 'json',
-       //     root: 'results',
-       //     totalProperty: 'total'
-       // }
-       reader: new Ext.data.JsonReader({
-           root: 'results',
-           id: 'id',
-           fields: ['name', 'fachgebiet', 'position', 'supervisor', 'id']
-       })
-    }
-});
-
-Ext.define('AM.model.fachgebiete.FachgebieteData', {
-    extend: 'Ext.data.Model',
-    fields:['id', 'name', 'budget', 'leiter', 'stellv'],
-    proxy: {
-		type: 'jsonp',
-		api: {
-            create: 'ws/fachgebiete/insert', // Called when saving new records
-            read: 'ws/fachgebiete', // Called when reading existing records
-            update: 'ws/fachgebiete/update', // Called when updating existing records
-            destroy: 'ws/fachgebiete/delete' // Called when deleting existing records
-		},        
-		reader: {
-            type: 'json',
-            root: 'results',
-            totalProperty: 'total'
-		},
-		writer: {
-		    type: 'json'
-		}  
-    }
-});
-
-/* ################## Menü ################### */
-Ext.define('AM.model.MenuModel', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'id', type: 'string'},
-        {name: 'text', type: 'string'},
-        {name: 'handler',type: 'string'},
-        {name: 'xtype',type: 'string'}
-    ],
-    
-    proxy: {
-        type: 'jsonp',
-        url: 'ws/menu',
-        reader: {
-            type: 'json',
-            root: 'results'
-        }
-    }    
-});
-
-
-/* ################## Columns (ContentGrid) ################### */
-Ext.define('AM.model.ColumnsFachgebiete', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'id', type: 'string'},
-        {name: 'text', type: 'string'},
-        {name: 'handler',type: 'string'},
-        {name: 'xtype',type: 'string'}
-    ],
-    
-    proxy: {
-        type: 'ajax',
-        url: 'resources/data/columnsFachgebiete.json',
-        reader: {
-            type: 'json',
-            root: 'columns'
-        }
-    }    
-});
-
-
-/* ################## HiWi ################### */
-Ext.define('AM.model.DashboardData', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'id', type: 'string'},
-        {name: 'priority', type: 'int'},
-        {name: 'deadline', type: 'date', dateFormat: 'd.m.y'},
-        {name: 'title',type: 'string'},
-        {name: 'assignedFrom',type: 'string'},
-        {name: 'assignedAt',type: 'date', dateFormat: 'd.m.y'},
-        {name: 'worked',type: 'string'}
-    ],
-    
-    proxy: {
-        // type: 'ajax',
-        // url: 'resources/data/dashboardData.json',
-        type: 'jsonp',
-        url: 'ws/aufgaben',
-        reader: {
-            type: 'json',
-            root: 'results'
-        }
-    }    
-});
-
-Ext.define('AM.model.TaskDetails', {
-    extend: 'Ext.data.Model',
-    fields: [
-        {name: 'id', type: 'int', useNull: true},
-        {name: 'date', type: 'date', dateFormat: 'd.m.y'},
-        {name: 'worked',type: 'string'},
-        {name: 'description', type: 'string'},       
-    ],
-    
-    proxy: {
-        type: 'jsonp',
-        api: {
-            create: 'ws/aufgabendetails/insert', // Called when saving new records
-            read: 'ws/aufgabendetails', // Called when reading existing records
-            update: 'ws/aufgabendetails/update', // Called when updating existing records
-            destroy: 'ws/aufgabendetails/delete' // Called when deleting existing records
-        },        
-        reader: {
-            type: 'json',
-            root: 'results'
-        },
-        writer: {
-            type: 'json'
-        }        
-    }    
-});
-
-
-/* ################## Mitarbeiter ################### */
-
-// Lets override JsonP-Proxy to change records=
+// Lets override JsonP-Proxy to change "records="
 Ext.define('Ext.data.proxy.JsonP', {
     extend: 'Ext.data.proxy.Server',
     alternateClassName: 'Ext.data.ScriptTagProxy',
@@ -358,6 +187,177 @@ Ext.define('Ext.data.proxy.JsonP', {
         return encoded;
     }
 });
+
+/**
+ * ! Wenn sich etwas am Model Ã¤ndert mÃ¼ssen evtl auch andere Stellen angepasst werden, da diese 
+ * auf die Felder ja zugreifen (z.B. die Detail-Fenster zu Personen bei Doppelklick @ Controller) 
+ * */
+
+Ext.define('AM.model.Fachgebiete', {
+    extend: 'Ext.data.Model',
+    fields:['name'],
+	proxy: {
+	    type: 'jsonp',
+	    url: 'ws/fachgebiete',
+	    reader: {
+	        type: 'json',
+	        root: 'results'
+	    }
+	}
+});
+
+Ext.define('AM.model.Personentypen', {
+    extend: 'Ext.data.Model',
+    fields:['name'],
+    proxy: {
+        type: 'ajax',
+        url: 'resources/data/personentypen.json',
+        reader: {
+            type: 'json',
+            root: 'results'
+        }
+    }
+});
+
+
+Ext.define('AM.model.Personen', {
+    extend: 'Ext.data.Model',
+    fields:['id', 'name', 'fachgebiet', 'position', 'supervisor'],
+    proxy: {
+        type: 'jsonp',
+        url: 'ws/personen',
+        format: 'json',
+       // reader: {
+       //     type: 'json',
+       //     root: 'results',
+       //     totalProperty: 'total'
+       // }
+       reader: new Ext.data.JsonReader({
+           root: 'results',
+           id: 'id',
+           fields: ['name', 'fachgebiet', 'position', 'supervisor', 'id']
+       })
+    }
+});
+
+Ext.define('AM.model.fachgebiete.FachgebieteData', {
+    extend: 'Ext.data.Model',
+    fields:['id', 'name', 'budget', 'leiter', 'stellv'],
+    proxy: {
+		type: 'jsonp',
+		api: {
+            create: 'ws/fachgebiete/insert', // Called when saving new records
+            read: 'ws/fachgebiete', // Called when reading existing records
+            update: 'ws/fachgebiete/update', // Called when updating existing records
+            destroy: 'ws/fachgebiete/delete' // Called when deleting existing records
+		},        
+		reader: {
+            type: 'json',
+            root: 'results',
+            totalProperty: 'total'
+		},
+		writer: {
+		    type: 'json'
+		}  
+    }
+});
+
+/* ################## Menü ################### */
+Ext.define('AM.model.MenuModel', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'id', type: 'string'},
+        {name: 'text', type: 'string'},
+        {name: 'handler',type: 'string'},
+        {name: 'xtype',type: 'string'}
+    ],
+    
+    proxy: {
+        type: 'jsonp',
+        url: 'ws/menu',
+        reader: {
+            type: 'json',
+            root: 'results'
+        }
+    }    
+});
+
+
+/* ################## Columns (ContentGrid) ################### */
+Ext.define('AM.model.ColumnsFachgebiete', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'id', type: 'string'},
+        {name: 'text', type: 'string'},
+        {name: 'handler',type: 'string'},
+        {name: 'xtype',type: 'string'}
+    ],
+    
+    proxy: {
+        type: 'ajax',
+        url: 'resources/data/columnsFachgebiete.json',
+        reader: {
+            type: 'json',
+            root: 'columns'
+        }
+    }    
+});
+
+
+/* ################## HiWi ################### */
+Ext.define('AM.model.DashboardData', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'id', type: 'string'},
+        {name: 'priority', type: 'int'},
+        {name: 'deadline', type: 'date', dateFormat: 'd.m.y'},
+        {name: 'title',type: 'string'},
+        {name: 'assignedFrom',type: 'string'},
+        {name: 'assignedAt',type: 'date', dateFormat: 'd.m.y'},
+        {name: 'worked',type: 'string'}
+    ],
+    
+    proxy: {
+        // type: 'ajax',
+        // url: 'resources/data/dashboardData.json',
+        type: 'jsonp',
+        url: 'ws/aufgaben',
+        reader: {
+            type: 'json',
+            root: 'results'
+        }
+    }    
+});
+
+Ext.define('AM.model.TaskDetails', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'id', type: 'int', useNull: true},
+        {name: 'date', type: 'date', dateFormat: 'd.m.y'},
+        {name: 'worked',type: 'string'},
+        {name: 'description', type: 'string'},       
+    ],
+    
+    proxy: {
+        type: 'jsonp',
+        api: {
+            create: 'ws/aufgabendetails/insert', // Called when saving new records
+            read: 'ws/aufgabendetails', // Called when reading existing records
+            update: 'ws/aufgabendetails/update', // Called when updating existing records
+            destroy: 'ws/aufgabendetails/delete' // Called when deleting existing records
+        },        
+        reader: {
+            type: 'json',
+            root: 'results'
+        },
+        writer: {
+            type: 'json'
+        }        
+    }    
+});
+
+
+/* ################## Mitarbeiter ################### */
 
 Ext.define('AM.model.HiWiAufgabe', {
     extend: 'Ext.data.Model',
